8de17be1edcf41574d1ab9e4931dc9639b3d2c86
Denis <dineresc@gmail.com>
2021-04-21 10:26:56 -0300

    add auth token

--- api/views.py
+++ api/views.py
@@ -1,12 +1,14 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.authentication import TokenAuthentication
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = (MovieSerializer)
+    authentication_classes = (TokenAuthentication,)
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
@@ -16,11 +18,10 @@ class MovieViewSet(viewsets.ModelViewSet):
             user = request.user
 
             try:
-                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating = Rating.objects.get(user=1, movie=movie.id)
                 rating.stars = stars
                 rating.save()
                 serializer = RatingSerializer(rating, many=False)
-                
                 response = {'message': 'Rating updated', 'result': serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
 
@@ -29,7 +30,11 @@ class MovieViewSet(viewsets.ModelViewSet):
                 serializer = RatingSerializer(rating, many=False)
                 response = {'message': 'Rating updated', 'result': serializer.data}
                 return Response(response, status=status.HTTP_200_OK)
-                 
+        else:
+            response = {'message': 'You need to provide the number of stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = (RatingSerializer)
+    authentication_classes = (TokenAuthentication,)
